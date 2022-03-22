e9adf4270908797ee420eff67133607381abf6a5
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2022-03-22 16:14:07 -0500

    NodeJS Backend and login authentication

--- App/lib/login.dart
+++ App/lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -21,19 +22,18 @@ class _Login extends State<Login> {
   }
 
   Column loginStructure() {
+    TextEditingController emailController = new TextEditingController();
+    TextEditingController pswController = new TextEditingController();
     return Column(
       children: <Widget>[
-
-         const Text(
+        const Text(
           "Login",
           style: TextStyle(fontSize: 50),
         ),
-
         const SizedBox(
           height: 20,
         ),
-
-         Text.rich(
+        Text.rich(
           TextSpan(
             text: "Don't have an account? ",
             style: const TextStyle(fontSize: 20),
@@ -41,43 +41,41 @@ class _Login extends State<Login> {
               TextSpan(
                   text: 'Sing In here',
                   style: TextStyle(
-                    decoration: TextDecoration.underline, decorationColor: Theme.of(context).colorScheme.primary,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Theme.of(context).colorScheme.primary,
                   )),
             ],
           ),
         ),
-
         const SizedBox(
           height: 60,
         ),
-
-        const TextField(
-          decoration: InputDecoration(
+        TextField(
+          controller: emailController,
+          decoration: const InputDecoration(
             labelText: "Email",
             labelStyle: TextStyle(fontSize: 20),
             filled: true,
           ),
         ),
-
         const SizedBox(
           height: 60,
         ),
-
-        const TextField(
-          decoration: InputDecoration(
+        TextField(
+          controller: pswController,
+          decoration: const InputDecoration(
             labelText: "Password",
             labelStyle: TextStyle(fontSize: 20),
             filled: true,
           ),
         ),
-
         const SizedBox(
           height: 60,
         ),
-
         ElevatedButton(
           child: const Text("Log in"),
-          onPressed: () {},
+          onPressed: () =>
+              {authenticate(emailController.text, pswController.text)},
           style: ElevatedButton.styleFrom(
             textStyle: const TextStyle(fontSize: 20),
             primary: Theme.of(context).colorScheme.primary,
@@ -86,5 +84,22 @@ class _Login extends State<Login> {
         ),
       ],
     );
+  }
+
+  void authenticate(String username, String password) async {
+    String uri = 'http://10.0.2.2:3000/api/users/'+username+'/'+password;
+    print(uri);
+    final response = await http
+        .get(Uri.parse(uri));
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      print(response.body);
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      print("Backend server error");
+    }
   }
 }
