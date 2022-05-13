47ebc72ef408b4c72c4a41777a8e9f1e756988c4
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2022-05-13 10:32:26 -0500

    Timer max seconds to 900

--- lib/Views/timer.dart
+++ lib/Views/timer.dart
@@ -5,18 +5,15 @@ import 'package:w_health/Elements/button_widget.dart';
 
 class TimerView extends StatefulWidget {
   @override
-    _Timer createState() => _Timer();
-
+  _Timer createState() => _Timer();
 }
 
 class _Timer extends State<TimerView> {
-  static const maxSeconds = 60;
+  static const maxSeconds = 900;
   int seconds = maxSeconds;
   Timer? timer;
 
   void resetTimer() => setState(() => seconds = maxSeconds);
-
-
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,62 +21,52 @@ class _Timer extends State<TimerView> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-              buildTime(),
-              SizedBox(height: 50),     
-              buildButtons()
-          ],
+          children: [buildTime(), SizedBox(height: 50), buildButtons()],
         ),
       ),
     );
-    }
+  }
 
-  Widget buildTime(){
+  Widget buildTime() {
     return Text(
-     '$seconds', 
-     style: TextStyle(
-       fontWeight: FontWeight.bold,
-       color: Theme.of(context).colorScheme.onSecondary,
-       fontSize: 80
-     ),
+      '$seconds',
+      style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Theme.of(context).colorScheme.onSecondary,
+          fontSize: 80),
     );
   }
 
-  void startTimer(){
-    timer = Timer.periodic(Duration(seconds: 1), (_) { 
-      if(seconds > 0){
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (_) {
+      if (seconds > 0) {
         setState(() => seconds--);
-      }
-      else {
+      } else {
         stopTimer(reset: false);
       }
     });
   }
 
-  void stopTimer({bool reset = true}){
-    if(reset){
+  void stopTimer({bool reset = true}) {
+    if (reset) {
       resetTimer();
     }
 
     setState(() => timer?.cancel());
   }
 
-  Widget buildButtons(){
+  Widget buildButtons() {
     final isRunning = timer == null ? false : timer!.isActive;
     final isCompleted = seconds == maxSeconds || seconds == 0;
     return isRunning || !isCompleted
         ? Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ButtonWidget(text: "Cancel", onClicked: stopTimer)
-          ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [ButtonWidget(text: "Cancel", onClicked: stopTimer)],
           )
         : ButtonWidget(
             text: "Start Timer",
-             onClicked: (){
-                startTimer();
-             });
+            onClicked: () {
+              startTimer();
+            });
   }
-
-  
 }
